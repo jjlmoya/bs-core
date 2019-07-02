dd7054833441ba38db3c1b18fab0e9a87e59fb21
juanjo1989@gmail.com <juanjo1989@gmail.com>
2019-07-02 20:56:44 +0200

    Improve Post Service

--- services/PostService.php
+++ services/PostService.php
@@ -7,26 +7,18 @@ class PostService
     public $description;
     public $url;
     public $words;
+    public $html;
 
     public function __construct($words = 30)
     {
         //global post used here
-
+        $query = get_post(get_the_ID());
         $this->words = $words;
         $this->title = get_the_title();
-        $this->description = $this->get_content();
+        $this->description = wp_trim_words(get_the_content(), $this->words, '...');
         $this->image = esc_url(get_the_post_thumbnail_url(get_the_ID()));
         $this->url = esc_url(get_the_permalink());
-    }
-
-    private function get_content()
-    {
-        if ($this->words > 300 || $this->words === -1) {
-            $query = get_post(get_the_ID());
-            return apply_filters('the_content', $query->post_content);
-        }
-
-        return wp_trim_words(get_the_content(), $this->words, '...');
+        $this->html = apply_filters('the_content', $query->post_content);
     }
 
     public function get_normalize_post()
@@ -34,7 +26,9 @@ class PostService
         return array(
             'title' => $this->title,
             'image' => $this->image,
-            'description' => $this->description
+            'description' => $this->description,
+            'html' => $this->html,
+            'url' => $this->url
         );
     }
 
