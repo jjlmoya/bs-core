66d19e66e50b6c5101103f98e029207b02492843
jjlmoya <juanjo1989@gmail.com>
2019-08-07 17:53:36 +0200

    PostService as a Param Post

--- services/PostService.php
+++ services/PostService.php
@@ -9,16 +9,32 @@ class PostService
     public $words;
     public $html;
 
-    public function __construct($words = 30)
+    public function __construct($post = null, $words = 30)
     {
-        //global post used here
-        $query = get_post(get_the_ID());
         $this->words = $words;
+        if (empty($post)) {
+            $this->get_global_post();
+        } else {
+            $this->get_post($post);
+        }
+    }
+
+    public function get_post($post)
+    {
+        $this->title = $post->post_title;
+        $this->description = wp_trim_words($post->post_content, $this->words, '...');
+        $this->image = esc_url(get_the_post_thumbnail_url($post->ID));
+        $this->url = esc_url(get_the_permalink($post->ID));
+    }
+
+    public function get_global_post()
+    {
+        $query = get_post(get_the_ID());
         $this->title = get_the_title();
         $this->description = wp_trim_words(get_the_content(), $this->words, '...');
         $this->image = esc_url(get_the_post_thumbnail_url(get_the_ID()));
         $this->url = esc_url(get_the_permalink());
-        $this->html = apply_filters('the_content', $query->post_content);
+        $this->html = $query->post_content;
     }
 
     public function get_normalize_post()
